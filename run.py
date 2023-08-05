fa0f26816a4a6a757416bcb66854d25a66b301c7
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-08-05 12:59:30 +0000

    Welcome logo and greeting. and the clear_screen & time sleep tested.

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+""" The retry of a new version of battleship game. """
+# importing randint,os and time libraries
+
+# from random import randint
+import os
+import time
+
+
+def clear_screen():
+    """Clear the terminal after sign in."""
+    os.system('clear')
+
+
+def logo():
+    """Welcome game logo"""
+    print(r"""
+   ___       __  __  __        __   _
+  / _ )___ _/ /_/ /_/ ___ ___ / /  (____
+ / _  / _ `/ __/ __/ / -_(_-</ _ \/ / _ \
+/____/\_,_/\__/\__/_/\__/___/_//_/_/ .__/
+                                  /_/
+""")
+
+
+def welcome():
+    """Welcome greeting"""
+    print("Welcome to my simple version of battleship!\n")
+#  calling the logo first and the greeting text.
+logo()
+welcome()
+
+
+#time.sleep(5)
+#clear_screen()
