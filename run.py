ab26f076144e19c5d6a9559c021604630786d4b4
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-08-05 18:54:55 +0000

    went through the linter,  a few white spaces  and indentations errors.

--- run.py
+++ run.py
@@ -68,30 +68,9 @@ def game_settings():
     size = game_board_size()
     ships = number_of_ships()
 
-
-def number_of_ships():
-    ships = 0
-    size = 0
-    while size < 5 or size > 8:
-        try:
-            size = int(input("Choose the number of rows/cols (5-8): "))
-            if size < 5 or size > 8:
-                print("invalid size. Please choose between 5 or 8?")
-        except ValueError:
-            print("Please enter a valid number between 5 and 8")
-    return size
-
-
-
-
-
-
-
-
-
-
-
 #  verify inputs of the board size 5x5 or 8x8?
+
+
 def game_board_size():
     size = 0
     while size < 5 or size > 8:
@@ -107,5 +86,21 @@ def game_board_size():
 board_size = game_board_size()
 print(f"test {board_size}")
 
+time.sleep(2)
+clear_screen()
 
 
+def number_of_ships():
+    ships = 0
+    while ships < 2 or size > 6:
+        try:
+            ships = int(input("How many ships would you like on the board?: "))
+            if ships < 2 or ships > 6:
+                print("invalid number of ships! Please choose between 2 or 6?")
+        except ValueError:
+            print("Please enter a valid number between 2 and 6")
+    return ships
+
+
+ships_on_the_board = number_of_ships()
+print(f"number of ships {ships_on_the_board}")
