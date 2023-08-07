918a8df4f39bc1fd2104f4ea14d72ce7dfa11530
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-08-07 09:37:12 +0000

    Updated: Test2.py: modified board size loop condition to only accept values 5 and 8.

--- testing/test2.py
+++ testing/test2.py
@@ -8,20 +8,14 @@ def game_settings():
 def game_board_size():
     """  verify inputs of the board size 5x5 or 8x8? """
     size = 0
-    while size < 5 or size > 8:
+    while size != 5 and size != 8:
         try:
             size = int(input("Choose the number of rows/cols (5-8): "))
-            if size < 5 or size > 8:
+            if size != 5 and size != 8:
                 print("invalid size. Please choose between 5 or 8?")
         except ValueError:
             print("Please enter a valid number between 5 and 8")
     return size
-
-
-BOARD_SIZE = game_board_size()
-print(f"test {BOARD_SIZE}")
-
-
 
 
 def build_board(size):
@@ -37,5 +31,9 @@ def print_board(board):
     for row in board:
         print(" ".join (row))
 
-BOARD_SIZE = game_settings()
-print_board(board)
+
+
+print(game_board_size())
+
+#BOARD_SIZE = game_board_size()
+#print(BOARD_SIZE)
