a9dd4aa2df02b3855c70feeec53d1036808b70bb
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-08-13 13:53:20 +0000

    Run.py: ADDED: some of the love sandwhich code, to compare and rewrite it. to fit the game.

--- run.py
+++ run.py
@@ -42,12 +42,26 @@ def welcome():
     print("Ahoy, sailor! Ready to conquer the seas?\n")
     print("in this simple version of battleship!\n")
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
 
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('Python Test-32b5fbe50b83.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
 
 gc = gspread.authorize(credentials)
 
